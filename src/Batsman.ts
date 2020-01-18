b75451dcbab1dff8921c96c20f005d25f6e3833a
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2020-01-18 18:07:13 +0000

    batsman can now score runs

--- src/Batsman.ts
+++ src/Batsman.ts
@@ -10,4 +10,18 @@ export class Batsman {
     this.runsScored = 0;
     this.ballsFaced = 0;
   }
+
+  // TODO: currently no extras/ no balls etc
+  addRuns(runs: number) {
+    if (runs < 0 || runs > 6) {
+      throw new Error('Illegal amount of runs ...');
+    }
+
+    this.runsScored += runs;
+    this.ballsFaced++;
+
+    console.log(
+      `${this.name} scored ${runs} runs, now ${this.runsScored}(${this.ballsFaced})`
+    );
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { Batsman } from './Batsman';
 
-const batsman = new Batsman('Tommy', 34);
+// TODO: Move tests from index.ts to /src/tests
+const tommy = new Batsman('Tommy', 34);
+
+tommy.addRuns(3);
+tommy.addRuns(4);
+tommy.addRuns(1);
+tommy.addRuns(0);
+
+console.log(tommy);
